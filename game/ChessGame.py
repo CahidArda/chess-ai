5f420e4f90cf0017eea7227c6bdc50de13e5455c
CahidArda <57228345+CahidArda@users.noreply.github.com>
2021-08-16 23:12:52 +0300

    Fix board output

--- game/ChessGame.py
+++ game/ChessGame.py
@@ -9,9 +9,9 @@ class ChessGame:
         self.players = players
 
     def run_game(self, n_moves = 10):
+        print(self.board)
         for i in range(n_moves):
-            print(self.board)
             self.board.apply_move(self.players[0].get_move(self.board))
             print(self.board)
             self.board.apply_move(self.players[1].get_move(self.board))
-        
+            print(self.board)

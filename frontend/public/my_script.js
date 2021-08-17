7d8deb54ffff65e03e71a63213e87c523f54a9ed
CahidArda <57228345+CahidArda@users.noreply.github.com>
2021-08-17 16:55:05 +0300

    Add forward and back buttons

--- frontend/public/my_script.js
+++ frontend/public/my_script.js
@@ -2,19 +2,29 @@ const board = document.querySelector('chess-board');
 
 const moves = ["d2-d4", "d7-d5", "d1-d3", "h7-h5", "b1-c3", "c8-g4", "f2-f3", "f7-f5", "c1-g5", "g8-h6", "f3-g4", "h5-g4", "d3-b5", "c7-c6", "b5-b7", "h6-f7", "g5-f4", "e7-e5", "d4-e5", "g7-g5", "f4-g3", "b8-d7", "b7-c6", "f8-b4", "c6-e6", "d8-e7", "e6-f5", "f7-e5", "g3-e5", "d7-e5", "g1-h3", "g4-h3", "e2-e4", "b4-c3", "b2-c3", "d5-e4", "f1-b5", "e8-d8", "a1-d1", "d8-c7"]
 
+const forward = () => {
+    board.move(moves[i])
+    i = i+1
+}
+
+const back = () => {
+    i = i-1
+    last_move = moves[i]
+    reversed_move = last_move.substring(3,5) + "-" + last_move.substring(0,2) 
+    board.move(reversed_move)
+}
+
 var pause = false
 var i = 0
 play_to_the_end = () => {
     console.log(i)
     setTimeout(function() {
         if (!pause) {
-            board.move(moves[i])
-            i = i+1
+            forward()
             if (i<moves.length) {
                 play_to_the_end()
             }
         }
-        
     }, 500)
 }
 
@@ -25,4 +35,12 @@ document.querySelector('#play_to_the_end').addEventListener('click', () => {
 
 document.querySelector('#pause').addEventListener('click', () => {
     pause = true
+});
+
+document.querySelector('#forward').addEventListener('click', () => {
+    forward()
+});
+
+document.querySelector('#back').addEventListener('click', () => {
+    back()
 });
